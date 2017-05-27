ec40f2c93a93fb686d6acbc433e6c6b2a817a047
edisonhello <edisonhello@hotmail.com>
2017-05-27 21:13:31 +0800

    add edit and delete feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var config = require('./config.json')
 var port = config.port || 3000
 
+var fs = require('fs')
 var express = require('express')
 var app = express()
 var colors = require('colors')
@@ -32,6 +33,12 @@ app.get('/short', (req, res) => {
 })
 app.get('/short/:id', (req, res) => {
     meow('LOG', 'Get request /short/'+req.params.id)
+    if(fs.existsSync(__dirname+'/pages/'+req.params.id+'.html')){
+        res.sendFile(__dirname+'/pages/'+req.params.id+'.html', () => {
+            res.end()
+        })
+        return
+    }
     urlDetail.findOne({ shortUrl: req.params.id }, (err, item) => {
         if(err) throw err
         if(item){
@@ -63,7 +70,7 @@ app.post('/', (req, res) => {
     meow('LOG', 'correct password')
     let shortUrl = req.body.short || randomstring.generate(4)
     urlDetail.findOne({ url: req.body.detail }, (err, item) => {
-        if(err) throw err;
+        if(err) throw err
         if(item){
             meow('DEBUG', 'find same: '+item)
             let link = 'https://weak.infor.org/short/'+item.shortUrl
@@ -87,6 +94,51 @@ app.post('/', (req, res) => {
                 meow('LOG', 'Saved!')
             })
         }
+    })
+})
+app.post('/edit', (req, res) => {
+    meow('LOG', 'Get post /edit')
+    if(sha512_256(req.body.pass) !== config.password){
+        meow('LOG', 'Wrong password')
+        res.send('Invaild password')
+        res.end()
+        return
+    }
+    meow('LOG', 'correct password')
+    urlDetail.findOne({ shortUrl: req.body.short }, (err, item) => {
+        if(err) throw err
+        if(item){
+            item.isUrl = req.body.isUrl
+            item.url = req.body.detail
+            item.save((err, item) => {
+                if(err) throw err
+                let link = 'https://weak.infor.org/short/'+req.body.short
+                res.send('<a href="'+link+'">'+link+'</a>')
+                res.end()
+                meow('LOG', 'Saved!')
+            })
+        }
+        else{
+            res.send('Nothing to edit!')
+            res.end()
+        }
+    })
+})
+app.post('/del', (req, res) => {
+    meow('LOG', 'Get post /del')
+    if(sha512_256(req.body.pass) !== config.password){
+        meow('LOG', 'Wrong password')
+        res.send('Invaild password')
+        res.end()
+        return
+    }
+    meow('LOG', 'correct password')
+    urlDetail.remove({ shortUrl: req.body.short }, (err) => {
+        if(err) throw err
+        meow('LOG', 'deleted')
+        res.send('Deleted')
+        res.end()
+        return
     })
 })
 
