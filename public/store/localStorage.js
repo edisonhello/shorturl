5d88705cdc75574fe0088cc79f8dd59d61cef032
edisonhello <edisonhello@hotmail.com>
2021-03-16 11:03:27 +0800

    Small refactor

--- public/store/localStorage.js
+++ public/store/localStorage.js
@@ -1,18 +1,41 @@
 
 import { set as setStorage, get as getStorage } from '../utils/localStorage.js';
 
-export function storeStateOnMutate(keyName, mutation, state) {
-  setStorage(`vuex.${keyName}`, JSON.stringify(state[keyName]));
+export function getKeyName(keyName) {
+  return `vuex.${keyName}`;
 }
 
-export function getStoreStateOnMutate(keyName) {
-  return (mutation, state) => storeStateOnMutate(keyName, mutation, state);
+/**
+ * 
+ * @param {string} moduleName 
+ * @param {*} mutation 
+ * @param {*} state Global state of vuex
+ */
+export function storeStateOnMutate(moduleName, mutation, state) {
+  setStorage(getKeyName(moduleName), JSON.stringify(state[moduleName]));
 }
 
-export function restoreState(keyName, state) {
-  this.replaceState(Object.assign(state, { [keyName]: JSON.parse(getStorage(`vuex.${keyName}`)) }));
+/**
+ * Returns a function that will store state in local storage with given module name
+ * @param {string} moduleName 
+ */
+export function getStoreStateOnMutate(moduleName) {
+  return (mutation, state) => storeStateOnMutate(moduleName, mutation, state);
 }
 
-export function getRestoreState (keyName) {
-  return function (state) { restoreState.bind(this)(keyName, state); }
+/**
+ * 
+ * @param {string} moduleName 
+ * @param {*} state Global state of vuex
+ */
+export function restoreState(moduleName, state) {
+  this.replaceState(Object.assign(state, { [moduleName]: JSON.parse(getStorage(getKeyName(moduleName))) }));
+}
+
+/**
+ * Returns a function that restore the state based on the value stored in local storage
+ * @param {string} moduleName 
+ */
+export function getRestoreState (moduleName) {
+  return function (state) { restoreState.bind(this)(moduleName, state); }
 }
